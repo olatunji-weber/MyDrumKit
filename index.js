e72ccaa77e921fe35f85764c350608b2645de31d
Olatunji Olayinka <olatunji.weber@gmail.com>
2022-03-12 17:42:13 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -2,7 +2,46 @@
 var buttons = document.querySelectorAll(".drum"); //returns an array of the button objects
 for (var i = 0; i < buttons.length; i++){    //loop through the elements of the buttons array adding event listeners all through
   buttons[i].addEventListener("click", function(){
-    var sound = new Audio("./sounds/tom-1.mp3");
-    sound.play();
+    switch (this.innerHTML) {
+      case "w":
+        var sound = new Audio("sounds/tom-1.mp3");
+        sound.play();
+        break;
+      case "a":
+        var sound = new Audio("sounds/tom-2.mp3");
+        sound.play();
+        break;
+      case "s":
+        var sound = new Audio("sounds/tom-3.mp3");
+        sound.play();
+        break;
+      case "d":
+        var sound = new Audio("sounds/tom-4.mp3");
+        sound.play();
+        break;
+      case "j":
+        var sound = new Audio("sounds/snare.mp3");
+        sound.play();
+        break;
+      case "k":
+        var sound = new Audio("sounds/kick-bass.mp3");
+        sound.play();
+        break;
+      case "l":
+        var sound = new Audio("sounds/crash.mp3");
+        sound.play();
+        break;
+      default: alert("You Clicked or Pressed " + this.innerHTML);
+
+    }
   });
 }
+
+
+// this.innerHTML = "<span style='color:#FFFFFF'> w </span>"
+// this.innerHTML = "<span style='color:#FFFFFF'> "+ this.innerHTML +" </span>";
+
+// var sound = new Audio("./sounds/tom-1.mp3");
+// sound.play();
+
+// this.style.color = #ffffff;
